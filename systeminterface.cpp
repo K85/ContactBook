f12b52ec58db4f341da5f39a27fbdbf115f190af
HakuzosuSAMA <3172906506@qq.com>
2021-07-13 16:22:40 +0800

    FIX Trim

--- systeminterface.cpp
+++ systeminterface.cpp
@@ -137,4 +137,8 @@ string &SystemInterface::trim(string &str) {
   if (str.empty()) {
     return str;
   }
+
+  str.erase(0, str.find_first_not_of(" "));
+  str.erase(str.find_last_not_of(" ") + 1);
+  return str;
 }
